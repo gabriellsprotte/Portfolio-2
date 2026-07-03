40fd8ec3474f922a6ddfb3a7ffe37f7de352b2ad
gabriellsprotte <joaao2108@gmail.com>
2026-07-03 08:59:30 -0300

    Minha primeira versao do site

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Portfolio-2/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
